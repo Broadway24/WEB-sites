d81b2de91a1f97dec565d3453c47a07ef6c3ed7e
Vladimir Rallo <lvadimir.rallo@yandex.ru>
2018-04-07 01:38:00 +0700

    add mirallis part1

---
+++ Mirillis/js/script.js
@@ -0,0 +1,9 @@
+menu.onclick = function myFunction() {
+	let x = document.getElementById("myTop-nav");
+
+	if (x.className === "top-nav") {
+		x.className += " responsive";
+	} else {
+		x.className = "top-nav";
+	}
+}
